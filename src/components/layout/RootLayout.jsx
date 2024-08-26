41a77727b33a06d24c33205229995d5a9bdafc41
caesiumy <dbs2636@gmail.com>
2024-08-26 11:29:41 +0900

    feat: 루트 레이아웃 추가

---
+++ src/components/layout/RootLayout.jsx
@@ -0,0 +1,11 @@
+const RootLayout = ({ children }) => {
+  return (
+    <main className="main-center">
+      <header>나는 완전히 붕괴되었어요</header>
+      {children}
+      <footer>저는 footer예요</footer>
+    </main>
+  );
+};
+
+export default RootLayout;
