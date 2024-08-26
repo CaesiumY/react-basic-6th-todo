5ca1008987307818934c8befaa98ae172d5ce824
caesiumy <dbs2636@gmail.com>
2024-08-26 11:52:48 +0900

    style: RootLayout에 스타일 추가

--- src/components/layout/RootLayout.jsx
+++ src/components/layout/RootLayout.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 
+// Compositional Layout - Apple
 const RootLayout = ({ children }) => {
   return (
     <RootLayoutMain>
@@ -12,6 +13,11 @@ export default RootLayout;
 
 const RootLayoutMain = styled.main`
   background-color: #f7f6f9;
+  min-height: 100vh;
 `;
 
-const RootLayoutContents = styled.div``;
+const RootLayoutContents = styled.div`
+  max-width: 768px;
+  margin: 0 auto;
+  padding: 20px;
+`;
