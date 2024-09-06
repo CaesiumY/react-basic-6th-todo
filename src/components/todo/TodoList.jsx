a36298dd6b16a2274cb798ce97367f77ded61eaa
caesiumy <dbs2636@gmail.com>
2024-09-06 12:47:36 +0900

    feat: 쿼리 스트링으로 투두 리스트 필터링

--- src/components/todo/TodoList.jsx
+++ src/components/todo/TodoList.jsx
@@ -2,9 +2,24 @@ import styled from "styled-components";
 import TodoItem from "./TodoItem";
 import { useContext } from "react";
 import { TodoContext } from "../../context/TodoContext";
+import { useSearchParams } from "react-router-dom";
 
 const TodoList = () => {
-  const { todos } = useContext(TodoContext);
+  const { todos, completedTodos, pendingTodos } = useContext(TodoContext);
+  const [searchParams] = useSearchParams();
+
+  const getFilteredTodos = () => {
+    const filter = searchParams.get("filter");
+
+    if (filter === "completed") {
+      return completedTodos;
+    } else if (filter === "pending") {
+      return pendingTodos;
+    }
+    return todos;
+  };
+
+  const filteredTodos = getFilteredTodos();
 
   return (
     <TaskSection>
@@ -12,7 +27,7 @@ const TodoList = () => {
         <h1>Tasks</h1>
       </TaskHeader>
       <TaskList>
-        {todos.map((todo) => (
+        {filteredTodos.map((todo) => (
           <TodoItem key={todo.id} todo={todo} />
         ))}
       </TaskList>
