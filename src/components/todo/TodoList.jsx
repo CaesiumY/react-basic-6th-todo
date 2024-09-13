f6664b95d49a4d470f32ef9d58c6a77b5b1587f9
caesiumy <dbs2636@gmail.com>
2024-09-13 12:27:13 +0900

    feat: 서버 상태 필터링 추가

--- src/components/todo/TodoList.jsx
+++ src/components/todo/TodoList.jsx
@@ -2,13 +2,15 @@ import { useQuery } from "@tanstack/react-query";
 import styled from "styled-components";
 import { getTodos } from "../../api/todoClient";
 import TodoItem from "./TodoItem";
+import { useSearchParams } from "react-router-dom";
 
 const TodoList = () => {
-  // const [searchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
+  const filter = searchParams.get("filter");
 
   const { data, isLoading, error } = useQuery({
-    queryKey: ["todos"],
-    queryFn: getTodos,
+    queryKey: ["todos", filter],
+    queryFn: () => getTodos(filter),
   });
 
   if (isLoading) {
