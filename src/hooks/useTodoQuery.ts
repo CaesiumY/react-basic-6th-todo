37b82529152211b653034e3b2c86cf1a53a08fda
caesiumy <dbs2636@gmail.com>
2024-09-27 12:49:40 +0900

    feat: 비동기 파트에 타입 추가

--- src/hooks/useTodoQuery.ts
+++ src/hooks/useTodoQuery.ts
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { getTodoDetail, getTodos } from "../api/todoClient";
 import { useGetFilter } from "./useGetFilter";
+import { Todo, TodoFilter } from "../types/todo.type";
 
 export const useTodoFilteredQuery = () => {
   const { filter } = useGetFilter();
@@ -11,14 +12,14 @@ export const useTodoFilteredQuery = () => {
   });
 };
 
-export const useTodoQuery = (filter) => {
+export const useTodoQuery = (filter: TodoFilter) => {
   return useQuery({
     queryKey: ["todos", filter],
     queryFn: () => getTodos(filter),
   });
 };
 
-export const useTodoDetailQuery = (id) => {
+export const useTodoDetailQuery = (id: Todo["id"]) => {
   return useQuery({
     queryKey: ["todos", id],
     queryFn: () => getTodoDetail(id),
