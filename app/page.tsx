e34999524ff8ebff6775066242d6ce4be92ca528
caesiumy <dbs2636@gmail.com>
2024-10-02 12:27:58 +0900

    feat: TodoForm 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import TodoForm from "@/components/todos/TodoForm";
 import TodoList from "@/components/todos/TodoList";
 
 const RootPage = () => {
   return (
     <main>
+      <TodoForm />
       <TodoList />
     </main>
   );
---
+++ components/todos/TodoForm.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { addTodo } from "@/api/todo-api";
+
+const TodoForm = () => {
+  const onSubmitTodo = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const title = formData.get("title")?.toString().trim();
+
+    if (!title) return;
+
+    await addTodo(title);
+
+    (e.target as HTMLFormElement).reset();
+  };
+
+  return (
+    <form onSubmit={onSubmitTodo}>
+      <input type="text" name="title" placeholder="할 일을 입력하세요" />
+      <button type="submit">추가</button>
+    </form>
+  );
+};
+
+export default TodoForm;
