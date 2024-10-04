8d8a38ed54493d036fb152a979d626a9ed73436f
caesiumy <dbs2636@gmail.com>
2024-10-04 12:38:04 +0900

    feat: Todo 완료됨 토글 추가

--- components/todos/TodoItem.tsx
+++ components/todos/TodoItem.tsx
@@ -1,6 +1,9 @@
 "use client";
 
-import { useDeleteTodoMutation } from "@/query/useTodoMutation";
+import {
+  useDeleteTodoMutation,
+  useToggleTodoMutation,
+} from "@/query/useTodoMutation";
 import { Todo } from "@/types/todo.types";
 import Link from "next/link";
 
@@ -10,14 +13,17 @@ interface TodoItemProps {
 
 const TodoItem = ({ todo }: TodoItemProps) => {
   const { mutate: deleteTodo } = useDeleteTodoMutation();
+  const { mutate: toggleTodo } = useToggleTodoMutation();
 
   const { id, completed, title } = todo;
 
   return (
     <div>
       <Link href={`/todo/${todo.id}`}>{title}</Link>-{" "}
-      {completed ? "완료됨" : "미완료"}
       <button onClick={() => deleteTodo(id)}>삭제</button>
+      <button onClick={() => toggleTodo({ id, completed: !completed })}>
+        {completed ? "완료됨" : "미완료"}
+      </button>
     </div>
   );
 };
