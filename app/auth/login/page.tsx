69f8eb97d0db8d472fe296c77acee1e00ac34cef
caesiumy <dbs2636@gmail.com>
2024-10-16 11:34:37 +0900

    feat: 로그인과 회원가입 폼 추가

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,12 @@
+import LoginForm from "@/components/auth/LoginForm";
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div className="min-h-screen flex items-center">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ app/auth/signup/page.tsx
@@ -0,0 +1,12 @@
+import { SignupForm } from "@/components/auth/SignupForm";
+import React from "react";
+
+const SignupPage = () => {
+  return (
+    <div className="min-h-screen flex items-center">
+      <SignupForm />
+    </div>
+  );
+};
+
+export default SignupPage;
